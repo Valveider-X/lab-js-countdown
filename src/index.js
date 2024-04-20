57c41a1fb0c74507d8eabf87088d35cc54f95ae9
Xavi <valcreativo@gmail.com>
2024-04-20 12:33:02 +0200

    casi solved lab

--- src/index.js
+++ src/index.js
@@ -6,14 +6,36 @@ let timer = null; // Variable to store the interval
 
 // ITERATION 1: Add event listener to the start button
 
-// Your code goes here ...
+// Your code goes here ... "start-btn"
 
-
+const startButton = document.querySelector("#start-btn");
+startButton.addEventListener("click",()  => {
+  startCountdown(10)
+})
+/*const deleteButton = contactNode.querySelector(".btn-delete");
+  deleteButton.addEventListener("click", () => {
+    contactNode.remove();
+  });*/
 
 
 // ITERATION 2: Start Countdown
-function startCountdown() {
+function startCountdown(seconds) {
+  let divTimer = document.querySelector("#time")
+  let counter = seconds;
   console.log("startCountdown called!");
+  const timer = setInterval(()=> {
+    startButton.disabled = true
+    divTimer.innerText --
+    counter--
+    console.log(counter)
+
+    if (counter <= 0){
+      clearInterval(timer)
+      showToast()
+
+    }
+
+  } ,1000)
 
 
   // Your code goes here ...
@@ -24,7 +46,19 @@ function startCountdown() {
 
 // ITERATION 3: Show Toast
 function showToast(message) {
-  console.log("showToast called!");
+  //console.log("showToast called!");
+  /*message - document.querySelector("#toast-message").innerText:
+  getElementById(selecciona la ID)
+
+  */
+  let toastCard = document.querySelector(".toast")
+  toastCard.classList.add("show") 
+  setTimeout( () => {
+    toastCard.classList.remove("show")
+  }, 3000)
+  //let messageToast = document.querySelector("#toast-message")
+  //messageToast.innerText = `pollarda`
+  //toastCard.classList.toggle("show")
 
   // Your code goes here ...
 
@@ -36,3 +70,4 @@ function showToast(message) {
   // Your code goes here ...
 
 }
+//showToast("pitosaurio")
